328afdc7ee078236d3feb571df7ab10bc0125626
David Anthoff <anthoff@berkeley.edu>
2023-05-18 10:24:06 -0700

    Fix bug

--- build.jl
+++ build.jl
@@ -2,7 +2,7 @@ using Pkg.Artifacts
 using Pkg.BinaryPlatforms
 using JSON
 import URIParser
-import NodeJS_18_jll
+import NodeJS
 
 package_dict = JSON.parsefile(joinpath(@__DIR__, "package.json"))
 pkgname = package_dict["name"]
@@ -45,8 +45,8 @@ platforms = [
     Windows(:x86_64),
 ]
 
-nodejs_cmd = NodeJS_18_jll.node()
-npm_cmd = Sys.iswindows() ? `$(string(NodeJS_18_jll.npm, ".cmd"))` : `$nodejs_cmd $(NodeJS_18_jll.npm)`
+nodejs_cmd = NodeJS.nodejs_cmd()
+npm_cmd = NodeJS.npm_cmd()
 
 for platform in platforms
 
